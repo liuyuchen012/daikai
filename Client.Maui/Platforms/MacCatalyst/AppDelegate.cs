eec1f000f8e224f8b343c2a3d3d4f62903051a75
liuyuchen012 <liuyuchen032901@outlook.com>
2026-07-13 00:54:31 +0800

    D:maui and build things

--- Client.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-using Foundation;
-
-namespace CheckIn.Client.Maui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
